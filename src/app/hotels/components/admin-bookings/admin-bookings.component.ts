d0a69e1996b9dea22f55893f5680acb43c5f291d
Sowmya K S <119122284+Sowmya-K-S@users.noreply.github.com>
2025-01-21 18:46:30 +0530

    create admin related components

---
+++ src/app/hotels/components/admin-bookings/admin-bookings.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-bookings',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-bookings.component.html',
+  styleUrl: './admin-bookings.component.css'
+})
+export class AdminBookingsComponent {
+
+}
---
+++ src/app/hotels/components/admin-hotels/admin-hotels.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-hotels',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-hotels.component.html',
+  styleUrl: './admin-hotels.component.css'
+})
+export class AdminHotelsComponent {
+
+}
